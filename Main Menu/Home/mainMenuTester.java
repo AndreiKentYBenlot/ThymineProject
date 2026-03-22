4cb3d004a568ccb6d4e0504d72492b37f645e451
JessNoelHAbanilla <jessnoel.abanilla@smc.pshs.edu.ph>
2026-03-22 21:07:54 +0800

    Add files via upload

---
+++ Main Menu/Home/mainMenuTester.java
@@ -0,0 +1,9 @@
+package PD;
+import Quarter2.*;
+
+public class mainMenuTester {
+    public static void main(String[] args) {
+        mainMenu go = new mainMenu();
+        go.setFrame();
+    }
+}
