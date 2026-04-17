1068f1b418053ba1681334ab4ead5e572947adaf
JessNoelHAbanilla <jessnoel.abanilla@smc.pshs.edu.ph>
2026-04-17 21:58:34 +0800

    Delete Main Menu/Home/mainMenuTester.java

--- Main Menu/Home/mainMenuTester.java
+++
@@ -1,9 +0,0 @@
-package PD;
-import Quarter2.*;
-
-public class mainMenuTester {
-    public static void main(String[] args) {
-        mainMenu go = new mainMenu();
-        go.setFrame();
-    }
-}
