bd1feb21bef4466ac686244f7fb791d50b56ade0
JessNoelHAbanilla <jessnoel.abanilla@smc.pshs.edu.ph>
2026-04-21 20:30:59 +0800

    Refactor CreditsMenu to use JButton with action listener
    Refactored CreditsMenu to use JButton instead of JLabel for the back button and added action listener functionality.

--- Main Menu/Credits/CreditsMenu.java
+++ Main Menu/Credits/CreditsMenu.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CreditsMenu  {
+public class CreditsMenu implements ActionListener {
     JFrame frame;
     JLabel display;
-    JLabel button1;
+    JButton button1;
     ImageIcon background;
     ImageIcon back;
     int frameWidth = 960;
@@ -17,12 +17,26 @@ public class CreditsMenu  {
         frame = new JFrame();
         back = new ImageIcon("PD Assets/back.png");
         background = new ImageIcon("PD Assets/backgroundCredits.gif");
-        button1 = new JLabel(back);
+        button1 = createIconButton(back);
         display = new JLabel(background);
         
         background = new ImageIcon(background.getImage().getScaledInstance((frameWidth),(frameHeight), Image.SCALE_DEFAULT));
         back = new ImageIcon(back.getImage().getScaledInstance((frameWidth/5),(frameHeight/9), Image.SCALE_DEFAULT));
         button1.setBackground(Color.black);
+        button1.addActionListener(this);
+    }
+    
+    private JButton createIconButton(ImageIcon icon) {
+        JButton btn = new JButton(icon);
+        btn.setBorderPainted(false);
+        btn.setContentAreaFilled(false);
+        btn.setFocusPainted(false);
+        return btn;
+    }
+  
+    private ImageIcon scaleImage(String path, int w, int h) {
+        Image img = new ImageIcon(path).getImage();
+        return new ImageIcon(img.getScaledInstance(w, h, Image.SCALE_SMOOTH));
     }
     
     public void setFrame() {
@@ -32,6 +46,14 @@ public class CreditsMenu  {
         frame.setSize(960,600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button1) {
+            frame.dispose();
+            mainMenu move = new mainMenu();
+            move.setFrame();
+        }
     }
     
     public static void main(String[] args) {
