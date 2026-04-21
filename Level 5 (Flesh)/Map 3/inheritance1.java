a5964a17fa5dd7961a8321b05dce8b4deb74fa71
CarellPSanchez <carell.sanchez@smc.pshs.edu.ph>
2026-04-21 08:57:36 +0800

    Add files via upload

---
+++ Level 5 (Flesh)/Map 3/inheritance1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CS4Q3;
+
+import javax.swing.*;
+
+public class inheritance1 {
+    
+    protected int position; 
+    
+    public inheritance1(int startPosition) {
+        this.position = startPosition;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int newPosition) {
+        position = newPosition;
+    }
+    
+}
