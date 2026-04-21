0b8f1aec319c1875be3ccbde425207d1b9c3aa3a
CarellPSanchez <carell.sanchez@smc.pshs.edu.ph>
2026-04-21 08:56:34 +0800

    Add files via upload

---
+++ Level 5 (Flesh)/Map 4/inheritance2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CS4Q3;
+
+public class inheritance2 extends inheritance1 {
+    
+    public inheritance2(int startPosition) {
+        super(startPosition); // calls parent constructor
+    }
+    
+}
+
