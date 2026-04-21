8d4c6f0a65e7f1dd861d5260ebb452704b02f0cb
JessNoelHAbanilla <jessnoel.abanilla@smc.pshs.edu.ph>
2026-04-21 20:29:05 +0800

    Update mainMenu.java

--- Main Menu/Home/mainMenu.java
+++ Main Menu/Home/mainMenu.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class mainMenu  {
+public class mainMenu implements ActionListener {
     JFrame frame;
     JLabel display;
-    JLabel button1;
-    JLabel button2;
-    JLabel button3;
+    JButton button1;
+    JButton button2;
+    JButton button3;
     ImageIcon background;
     ImageIcon newGame;
     ImageIcon map;
@@ -23,9 +23,9 @@ public class mainMenu  {
         map = new ImageIcon("PD Assets/map.png");
         credits = new ImageIcon("PD Assets/credits.png");
         background = new ImageIcon("PD Assets/background.gif");
-        button1 = new JLabel(newGame);
-        button2 = new JLabel(map);
-        button3 = new JLabel(credits);
+        button1 = createIconButton(newGame);
+        button2 = createIconButton(map);
+        button3 = createIconButton(credits);
         display = new JLabel(background);
         
         background = new ImageIcon(background.getImage().getScaledInstance((frameWidth),(frameHeight), Image.SCALE_DEFAULT));
@@ -35,6 +35,9 @@ public class mainMenu  {
         button1.setBackground(Color.black);
         button2.setBackground(Color.black);
         button3.setBackground(Color.black);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
     }
     
     public void setFrame() {
@@ -46,6 +49,38 @@ public class mainMenu  {
         frame.setSize(960,600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    
+    
+    private JButton createIconButton(ImageIcon icon) {
+        JButton btn = new JButton(icon);
+        btn.setBorderPainted(false);
+        btn.setContentAreaFilled(false);
+        btn.setFocusPainted(false);
+        return btn;
+    }
+  
+    private ImageIcon scaleImage(String path, int w, int h) {
+        Image img = new ImageIcon(path).getImage();
+        return new ImageIcon(img.getScaledInstance(w, h, Image.SCALE_SMOOTH));
+    }
+    
+    public void actionPerformed (ActionEvent e) {
+        if (e.getSource() == button1) {
+            frame.dispose();
+            CaveEntrance move = new CaveEntrance();
+            move.setFrame();
+        }
+        else if (e.getSource() == button2) {
+            frame.dispose();
+            MapMenu move = new MapMenu();
+            move.setFrame();
+        }
+        else if (e.getSource() == button3) {
+            frame.dispose();
+            CreditsMenu move = new CreditsMenu();
+            move.setFrame();
+        }
     }
     
     public static void main(String[] args) {
